29c8d4e2dc396f6c2696e5b1cbc2f4c81c8682a6
safae-ouchen <safaeouchen2004@gmail.com>
2025-11-11 18:08:04 +0100

    Add files via upload

---
+++ bank-service/src/main/java/ma/formations/graphql/service/model/GetTransactionListBo.java
@@ -0,0 +1,14 @@
+package ma.formations.graphql.service.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class GetTransactionListBo {
+    private String rib;
+    private Date dateTo;
+    private Date dateFrom;}
